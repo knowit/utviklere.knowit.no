90223704ab2126983648b14b020536dd7b7bdb3d
Ole Bakstad <ole.bakstad@knowit.no>
2016-04-24 21:05:54 +0200

    Add typeform window

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ function selectProject(e) {
 
   $(e).toggleClass('active');
 }
+
+function createTypeformWindow() {
+  $('#typeform-window').fadeIn();
+  $('#typeform-window iframe').attr('src', 'typeform.html');
+  $('#typeform-window iframe').load(function() {
+    $('iframe').fadeIn();
+  });
+}
+
+function closeTypeform() {
+  $("#typeform-window").fadeOut();
+}
+
+$(document).ready(function() {
+
+  $(".soknad-knapp a").click(function(e) {
+    createTypeformWindow();
+    e.stopPropagation();
+  });
+
+  $('body').click(function(e) {
+    closeTypeform();
+    e.stopPropagation();
+  });
+
+  $(document).on('keyup',function(evt) {
+    if (evt.keyCode == 27) {
+      closeTypeform();
+    }
+  });
+});
